a3d9811e981a2852c9c239e758c01e83e9f86a71
Black_P8 <pavel_romanov26@mail.ru>
2024-03-04 21:18:49 +0300

    finish task 3

---
+++ src/main/src/module1/Task3.kt
@@ -0,0 +1,12 @@
+package org.example.module1
+
+fun main() {
+    var five: Char = '5'
+    println(five.toString())
+    println(five.toByte())
+    println(five.toShort())
+    println(five.toInt())
+    println(five.toDouble())
+    println(five.toFloat())
+    println(five.toLong())
+}
